f8b72e67fb29d417cdd522c19c45d2edf4796041
kadir-zeren <k4dzeren@gmail.com>
2024-05-04 16:51:39 +0300

    python variables

---
+++ python.py
@@ -0,0 +1,129 @@
+a = "bu benim yazdigim string \
+stringe devam ediyorum"
+
+print(a)
+
+a="bu benim yazdigim string \nstringe devam ediyorum"
+
+print(a)
+
+school = "clarusway"
+print(school)
+
+sayi = 35
+print(sayi)
+
+my_age = 33
+your_age = 30
+my_age = your_age
+print(my_age)
+
+okul = 'Clarusway'
+print(okul)
+print(help('keywords'))
+
+_okul ='clarusway'
+print(_okul)
+
+variable = "degisken"
+isim_soyisim='ali guzel'
+
+degisken = 'bu bir string'
+print(degisken)
+
+degisken = 5
+print(degisken)
+
+x = y = z ="same"
+print(x)
+print(y)
+print(z)
+
+a= 'degisken_1'
+b= 5
+c =7.6
+print(a,b,c)
+
+january = march = may = july = august = october = december = 31
+april = june = september = november = 30
+february = 28
+print(january,february,march,april,may,june,july,august,september,october,november,december)
+
+ali = None
+print(ali)
+
+x = 15 
+z = 33
+z = x
+x = y
+
+print(x)
+print(y)
+print(z)
+
+a, b, c = 5, 7, 9
+print(a)
+print(b)
+print(c)
+
+a='clarusway'
+print(type(a))
+
+sehir = '35'
+print(type(sehir))
+
+str_number = '1923'
+print(type(str_number))
+
+sehir = '35'
+print(type(sehir))
+
+sayi1=40
+print(type(sayi1))
+
+a = 5.7
+print(type(a))
+
+b = True
+print(type(a),type(b))
+
+a = 123 
+print(type(a))
+print(type(str(a)))
+
+yeni_a = str(a)
+
+print(type(yeni_a))
+
+pi = 3.14
+converted_pi = str(pi)
+print(converted_pi)
+print(type(converted_pi))
+
+pi = '3.14'
+
+print(pi)
+
+a = 1.23
+print(type(a))
+print(a)
+print(int(a))
+
+tamsayi = 7
+print(type(tamsayi))
+print(float(tamsayi))
+print(type(tamsayi))
+print(type(float(tamsayi)))
+
+x = 39 
+v = "11"
+y = "2.5"
+z = "I am at_"
+
+print(x-int(v))
+print(x-float(y))
+print(z+str(x))
+
+print(5 + 3)
+print(5 + 3.0)
+print('123'+'12')
